d689f79040e46d3189d89a9f6b6af1094564c562
kkwang2 <doitjava@nate.com>
2022-08-23 17:51:27 +0900

    1

---
+++ 자료구조/P11720_숫자의합.java
@@ -0,0 +1,28 @@
+package algorithem.DataStructures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class P11720_숫자의합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String sNum = br.readLine();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+
+        for(int i=0; i<N; i++) {
+            sum += cNum[i] - '0';
+        }
+
+//        for(int i=0; i<N; i++) {
+//            sum += sNum.charAt(i) - '0';
+//        }
+
+        System.out.println(sum);
+
+    }
+
+}
